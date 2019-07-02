e89e6a38e46d384a8d55188cfe4e06923bebaafc
Lennard Wohlan <lennardwohlan@web.de>
2019-07-02 20:20:37 +0200

    Boulder: to long ago to remember what was changed

--- BoulderProject-Umbraco/App_Data/Models/all.generated.cs
+++ BoulderProject-Umbraco/App_Data/Models/all.generated.cs
@@ -8,8 +8,8 @@ using  Umbraco.Web;
 using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
-[assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "c0af750e3dd1beec")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
+[assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "374a991aa6d2a71")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.4")]
 
 
 // FILE: models.generated.cs
@@ -90,6 +90,20 @@ namespace Umbraco.Web.PublishedModels
 		{ }
 
 		// properties
+
+		///<summary>
+		/// Infotext
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder", "8.0.4")]
+		[ImplementPropertyType("infotext")]
+		public string Infotext => this.Value<string>("infotext");
+
+		///<summary>
+		/// Überschrift
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder", "8.0.4")]
+		[ImplementPropertyType("uberschrift")]
+		public string Uberschrift => this.Value<string>("uberschrift");
 	}
 
 	/// <summary>Folder</summary>
